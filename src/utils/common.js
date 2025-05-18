52bdde05b8e0202472c4cd72a7ade664fb83e983
Karla <karlaabdraeva@icloud.com>
2025-05-18 15:54:29 +0300

    module8-task3

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,7 +1,3 @@
-export function getRandomArrayElement(items) {
-  return items[Math.floor(Math.random() * items.length)];
-}
-
 export function createUpperCase(word) {
   return (`${word[0].toUpperCase()}${word.slice(1)}`);
 }
